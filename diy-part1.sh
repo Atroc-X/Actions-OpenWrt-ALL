8979354a06d99da58fcbb42f5c17c649a5d27b3b
Atroc X <93807457+Atroc-X@users.noreply.github.com>
2021-11-06 17:43:24 +0800

    Add files via upload

---
+++ diy-part1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/Atroc-0625/Actions-OpenWrt-All
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: Atroc-0625
+#=============================================================
+
+# Uncomment a feed source
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
+git clone https://github.com/sypopo/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
+#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+#sed -i 's/luci-theme-bootstrap/luci-theme-atmaterial/g' feeds/luci/collections/luci/Makefile
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
